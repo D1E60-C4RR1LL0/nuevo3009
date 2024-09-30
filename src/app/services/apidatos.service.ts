f3e29a1442fe49e3931566b77e7e7f7c2bdfb2f4
D1E60-C4RR1LL0 <di.carrillog@duocuc.cl>
2024-09-30 15:56:04 -0300

    App para Comida

---
+++ src/app/services/apidatos.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Categorias } from '../interfaces/icomidas';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApidatosService {
+
+  constructor(private httpclient:HttpClient) { }
+
+  getCategorias() {
+    return this.httpclient.get<Categorias>('www.themealdb.com/api/json/v1/1/categories.php');
+  }
+}
